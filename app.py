3823409676f380699202fa45360a9e7ad41f394e
giri prasad <giri.prasad@impelsys.com>
2022-05-11 23:02:31 +0530

    playing with git

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 a = "hello python"
 print(a)
+
+user_name = "giri prasad"
+password = 123
+user = input("Enter your name:")
+user_password =int(input("Enter password:"))
+if(user_name==user and password==user_password):
+    print("Loghin Successful")
+else:
+    print("Loggin failed")
